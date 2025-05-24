30bd97585bd73fe70fa2614815d42da404baeeca
1chgro <1chgr077@gmail.com>
2025-05-24 18:34:09 +0100

    .

--- minishell.h
+++ minishell.h
@@ -13,8 +13,6 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-#define DEBUGG_CHECK write(2, "got to this point successfully\n", 32);
-
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
@@ -67,43 +65,53 @@ typedef struct s_env{
     struct s_env *next;
 } t_env;
 
-void	msh_loop(char **envp);
-void	msh_signals();
-int		valid_quotes(char *s);
-void		skip_quotes(char *s, int *i, char q);
-t_token		*lexer(char *line);
-void free_arr(char **arr);
-void free_cmd(t_cmd *cmd);
+//----------------- print functions--------///
+void   print_cmd(t_cmd *cmd);
+void print_tokens(t_token *tokens);
 
-// tokenization functions
-t_token		*tokenize(char *line);
-void		add_token(t_token **head, t_token *new_token);
-t_token		*new_token(char *value, t_token_type type);
-t_token_type		get_token_type(char *value);
+
+//--------------- main functions -----------------------//
+void	msh_loop(char **envp);
+int   	msh_execute(t_cmd *cmd, t_env *env);
+void	msh_signals();
+t_token		*lexer(char *line);
+
+
+
+//------------------- free functions -----------------------//
+void		free_arr(char **arr);
+void		free_cmd(t_cmd *cmd);
 void		free_tokens(t_token *tokens);
 
-// syntax
+//------------------------ tokenization ---------------//
+t_token				*tokenize(char *line);
+t_token				*new_token(char *value, t_token_type type);
+void				add_token(t_token **head, t_token *new_token);
+t_token_type		get_token_type(char *value);
+
+
+//------------------------- syntax -----------------------//
 int check_syntax_err(t_token *tokens);
 
-// expand_env functions
+//--------------------- cmd create ----------------------------//
+t_cmd	*create_cmd(t_token *tokens, t_env *env);
 
+//------------------- env --------------------------------------//
+int		copie_env(t_env **c_env, char **env);
+char	*my_getenv2(char *name, t_env *env);
 
-//cmd create functions
-t_cmd *create_cmd(t_token *tokens, t_env *env);
-int copie_env(t_env **c_env, char **env);
-
-
-// Utility functions
+//-----------------------Utility functions-----------------//
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_stringcpy(char *dst, const char *src, size_t n);
-size_t	ft_strlen(const char *str);
 char	*ft_strdup(char *str);
-int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strjoin_ws(char const *s1, char const *s2);
+char	*ft_strndup(const char *s, int n);
+int		ft_strcmp(const char *s1, const char *s2);
 void	ft_putstr_fd(char *s, int fd);
-char *ft_strndup(const char *s, int n);
+size_t	ft_strlen(const char *str);
 
+//-------------------is functions-------------------//
 int	is_quote(char c);
 int	is_operator(char c);
 int	is_space(char c);
@@ -111,23 +119,27 @@ int	is_redirection(t_token **token);
 int	is_pipe(t_token **token);
 int	is_valid_char(char c);
 
-//-------------------expanding-------------------//
+//------------------- quotes -----------------------//
+void	skip_quotes(char *s, int *i, char q);
+char	*remove_outer_quotes(char *s);
+int		valid_quotes(char *s);
+char	**remove_quotes_arr(char **argv);
+
+//------------------- expanding -------------------//
 void	expand_env_vars(t_cmd *cmd, t_env *env);
 char	*expand(char *line, t_env *env);
-char	*remove_outer_quotes(char *s);
 char	*expand_heredoc(char *line, t_env *env);
-char	*my_getenv2(char *name, t_env *env);
 
-//--------------------------excution-------------------//
-int    msh_execute(t_cmd *cmd, t_env *env);
-int    run_cmd(t_cmd *cmd, t_env *env);
+
+//-------------------------- excution -------------------//
+int		run_cmd(t_cmd *cmd, t_env *env);
 int     copie_env(t_env **c_env, char **env);
 char    *ft_strchr(const char *s, int c);
 char    **ft_split(char const *s, char c);
-t_env *create_node(char *key, char *value);
-void append_node(t_env **head, t_env *node);
-int  are_builtin(char    *cmd);
-int	run_builtin(char **s_cmd, t_env *env);
+t_env	*create_node(char *key, char *value);
+void	append_node(t_env **head, t_env *node);
+int		are_builtin(char    *cmd);
+int		run_builtin(char **s_cmd, t_env *env);
 int    ft_pwd();
 int    ft_env(char **s_cmd, t_env *env);
 int    ft_exit(char **s_cmd);
@@ -136,7 +148,7 @@ int    ft_unset(char **s_cmd, t_env **env);
 long	ft_atoi(const char *str);
 int    ft_echo(char **s_cmd);
 int    ft_cd(char  **s_cmd, t_env  **env);
-void free_split(char **split);
+void  free_split(char **split);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 int redirection(t_cmd *cmd);
 char	*get_key(char	*str);
@@ -151,5 +163,5 @@ void update_node_value(t_env *node, char *value, int append);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putchar_fd(char c, int fd);
 t_env    *get_exit(t_env *env);
-void free_env(t_env *env);
+void	free_env(t_env *env);
 #endif
---
+++ parsing/free_functions.c
@@ -0,0 +1,56 @@
+#include "../minishell.h"
+
+void	free_tokens(t_token *tokens)
+{
+	t_token	*tmp;
+
+	while (tokens)
+	{
+		tmp = tokens;
+		tokens = tokens->next;
+		free(tmp->value);
+		tmp->value = NULL;
+		free(tmp);
+		tmp = NULL;
+	}
+}
+
+void free_arr(char **arr)
+{
+    int i = 0;
+    if (!arr)
+        return ;
+    while (arr[i])
+    {
+        free(arr[i]);
+        i++;
+    }
+    free(arr);
+}
+
+void free_cmd_files(t_red *files)
+{
+    int i = 0;
+    if (!files)
+        return ;
+    while (files[i].filename)
+    {
+        free(files[i].filename);
+        i++;
+    }
+    free(files);
+}
+
+void free_cmd(t_cmd *cmd)
+{
+    t_cmd *temp;
+    while (cmd)
+    {
+        free(cmd->line);
+        free_arr(cmd->argv);
+        free_cmd_files(cmd->files);
+        temp = cmd;
+        cmd = cmd->next;
+        free(temp);
+    }
+}
